db4f21e1aecac478a915ad177ffcf4030f7e17ec
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-10-21 21:26:01 +0100

    Added map component

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,28 @@
   import Gradients from "./lib/Gradients.svelte";
   import Portfolio from "./lib/Portfolio.svelte";
   import { Github, Linkedin, Twitter} from "lucide-svelte";
+  import Map from "./lib/Map.svelte";
+  const views = [Gradients, Map]
+
+  let currentView = 0
+  let viewportComponent = views[0]
+
+
+  function updateViewport() {
+      let newView = Math.floor(Math.random() * views.length);
+      if (newView === currentView) {
+          if (newView !== views.length) {
+              newView++;
+          } else {
+              newView--;
+          }
+      }
+      viewportComponent = views[Math.floor(Math.random() * views.length)]
+      currentView = newView
+  }
 </script>
 
-<div class="absolute m-auto w-full h-full content-center align-middle flex flex-col items-center justify-center">
+<div class="absolute m-auto w-full h-full content-center align-middle flex flex-col items-center justify-center z-50">
     <div>
         <div class="my-10">
            <h1 class="my-3 text-8xl">Hi, I'm Dan Hirst</h1>
@@ -16,13 +35,11 @@
             <a href="https://www.linkedin.com/in/dan-hirst/" class="btn btn-circle m-auto"><Linkedin/></a>
             <a href="https://twitter.com/dchirst0" class="btn btn-circle m-auto"><Twitter/></a>
         </div>
-        <button class="btn-primary m-5">Click</button>
+        <button class="btn-primary m-5" on:click={updateViewport}>Click</button>
     </div>
     <Portfolio/>
-
 </div>
-<Gradients/>
-
+<svelte:component this={viewportComponent}></svelte:component>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Josefin+Sans:wght@700&family=Truculenta:opsz,wght@12..72,500&display=swap');
