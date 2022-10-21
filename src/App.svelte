a5753df1ea32d876ce07c3a05fc59068cb777f89
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-10-21 21:45:38 +0100

    Moved text to own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,20 @@
 <script>
   import Gradients from "./lib/Gradients.svelte";
   import Portfolio from "./lib/Portfolio.svelte";
-  import { Github, Linkedin, Twitter} from "lucide-svelte";
   import Map from "./lib/Map.svelte";
+  import Content from "./lib/Content.svelte";
   const views = [Gradients, Map]
 
   let currentView = 0
-  let viewportComponent = views[0]
+  let viewportComponent;
+
+  $: viewportComponent = views[currentView]
 
 
-  function updateViewport() {
-      let newView = Math.floor(Math.random() * views.length);
-      if (newView === currentView) {
-          if (newView !== views.length) {
-              newView++;
-          } else {
-              newView--;
-          }
-      }
-      viewportComponent = views[Math.floor(Math.random() * views.length)]
-      currentView = newView
-  }
 </script>
 
 <div class="absolute m-auto w-full h-full content-center align-middle flex flex-col items-center justify-center z-50">
-    <div>
-        <div class="my-10">
-           <h1 class="my-3 text-8xl">Hi, I'm Dan Hirst</h1>
-            <h2 class="text-3xl">Developer, data scientist, map nerd</h2>
-        </div>
-
-        <div class="grid grid-cols-3 grid-rows-1 grid-flow-row gap-4 text-center m-5">
-            <a href="https://github.com/dchirst" class="btn btn-circle m-auto"><Github/></a>
-            <a href="https://www.linkedin.com/in/dan-hirst/" class="btn btn-circle m-auto"><Linkedin/></a>
-            <a href="https://twitter.com/dchirst0" class="btn btn-circle m-auto"><Twitter/></a>
-        </div>
-        <button class="btn-primary m-5" on:click={updateViewport}>Click</button>
-    </div>
+    <Content viewLength="{views.length}" bind:currentView="{currentView}"/>
     <Portfolio/>
 </div>
 <svelte:component this={viewportComponent}></svelte:component>
-
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Josefin+Sans:wght@700&family=Truculenta:opsz,wght@12..72,500&display=swap');
-    h1, h2 {
-        font-family: 'Abril Fatface', cursive;
-    }
-</style>
