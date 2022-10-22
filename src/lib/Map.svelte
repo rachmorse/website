ca1138077f917804835417e6985b75e6218eac23
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-10-22 11:39:44 +0100

    Fixed map bug

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,15 +1,17 @@
 <script>
     import { onMount } from "svelte";
     import * as L from "leaflet";
+    import 'leaflet/dist/leaflet.css';
+
 
     let map;
 
     onMount(() => {
-        map = L.map('map').setView([51.511867, -0.110367], 13);
+        map = L.map('map', {'zoomControl': false}).setView([51.511867, -0.110367], 13);
         L.tileLayer(`https://api.os.uk/maps/raster/v1/zxy/Light_3857/{z}/{x}/{y}.png?key=${import.meta.env.VITE_OS_API_KEY}`, {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            attribution: "Contains OS data &copy Crown copyright and database rights 2022"
         }).addTo(map);
 
     })
 </script>
-<div id="map" class="w-screen h-screen"></div>
+<div id="map" class="w-screen h-screen fixed top-0"></div>
